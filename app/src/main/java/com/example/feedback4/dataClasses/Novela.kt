3b0fb399cbf98f5cab78f22440b08bc3b1150fc5
Sira <sgonzmad@myuax.com>
2024-11-04 09:25:57 +0100

    dataClasses anteriores

---
+++ app/src/main/java/com/example/feedback4/dataClasses/Novela.kt
@@ -0,0 +1,10 @@
+package com.example.feedback4.dataClasses
+
+data class Novela(
+    val titulo: String,
+    val autor: String,
+    val anioPublicacion: Int,
+    val sinopsis: String,
+    var esFavorita: Boolean = false,
+    val resenas: MutableList<String> = mutableListOf()
+)
